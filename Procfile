7fad56b840b4a468850c28ea34b10e7400843f62
vinayKumarreddy-mangalampenta <mangalapentavinaykumarreddy30@gmail.com>
2022-12-04 21:46:49 +0530

    abcd

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn 'base.wsgi'
-
+web: python manage.py migrate && gunicorn base.wsgi
--- base/settings.py
+++ base/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
